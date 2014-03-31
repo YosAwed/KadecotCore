0ff433bc8ebc99829c0be6b5508483ff359b603f
Yusuke Seshita <yseshita@users.noreply.github.com>
2014-03-31 14:01:07 +0900

    Implement Wamp caller

---
+++ src/com/sonycsl/wamp/WampCaller.java
@@ -0,0 +1,26 @@
+
+package com.sonycsl.wamp;
+
+public abstract class WampCaller extends WampClient {
+
+    @Override
+    protected boolean consumeRoleMessage(WampMessenger friend, WampMessage msg) {
+        if (consumeMyMessage(friend, msg)) {
+            onConsumed(msg);
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean consumeMyMessage(WampMessenger friend, WampMessage msg) {
+        if (msg.isResultMessage()) {
+            result(msg.asResultMessage());
+            return true;
+        }
+        return false;
+    }
+
+    abstract protected void result(WampResultMessage msg);
+
+}
