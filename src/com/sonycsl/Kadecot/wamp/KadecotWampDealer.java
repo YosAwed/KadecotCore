339f8c070872340ff84a6a6a2e5f8b087f9bd8dc
Yusuke Seshita <yseshita@users.noreply.github.com>
2014-04-01 17:32:46 +0900

    Add KadecotWampDealer

---
+++ src/com/sonycsl/Kadecot/wamp/KadecotWampDealer.java
@@ -0,0 +1,13 @@
+
+package com.sonycsl.Kadecot.wamp;
+
+import com.sonycsl.wamp.WampDealer;
+import com.sonycsl.wamp.WampMessage;
+
+public class KadecotWampDealer extends WampDealer {
+
+    @Override
+    protected void onConsumed(WampMessage msg) {
+    }
+
+}
