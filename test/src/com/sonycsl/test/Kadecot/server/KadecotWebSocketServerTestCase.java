9597b04a39d15a04cb8d8442f51060ad9857d4c5
Kazuhito NAKAMURA <kazuhiton@users.noreply.github.com>
2014-04-02 17:03:07 +0900

    add KadecotWebSocketServerTestCase

---
+++ test/src/com/sonycsl/test/Kadecot/server/KadecotWebSocketServerTestCase.java
@@ -0,0 +1,56 @@
+
+package com.sonycsl.test.Kadecot.server;
+
+import android.content.Context;
+import android.test.mock.MockContext;
+
+import com.sonycsl.Kadecot.server.KadecotWebSocketServer;
+
+import junit.framework.TestCase;
+
+public class KadecotWebSocketServerTestCase extends TestCase {
+
+    private static class TestMockContext extends MockContext {
+
+        @Override
+        public Context getApplicationContext() {
+            return this;
+        }
+
+    }
+
+    private KadecotWebSocketServer mServer;
+
+    protected void setUp() throws Exception {
+        mServer = KadecotWebSocketServer.getInstance(new TestMockContext());
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        if (mServer.isStarted()) {
+            mServer.stop();
+        }
+    }
+
+    public void testCtor() {
+        assertNotNull(mServer);
+    }
+
+    public void testSingleton() {
+        assertEquals(mServer, KadecotWebSocketServer.getInstance(new TestMockContext()));
+    }
+
+    public void testStart() {
+        assertFalse(mServer.isStarted());
+        mServer.start();
+        assertTrue(mServer.isStarted());
+    }
+
+    public void testStop() {
+        assertFalse(mServer.isStarted());
+        mServer.start();
+        assertTrue(mServer.isStarted());
+        mServer.stop();
+        assertFalse(mServer.isStarted());
+    }
+}
