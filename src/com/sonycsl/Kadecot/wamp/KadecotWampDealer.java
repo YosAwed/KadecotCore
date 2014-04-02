f01e4d492d8921898917da7f1c26bb66b38ee19e
Yusuke Seshita <yseshita@users.noreply.github.com>
2014-04-02 12:55:30 +0900

    Add constructor of KadecotWampDealer

--- src/com/sonycsl/Kadecot/wamp/KadecotWampDealer.java
+++ src/com/sonycsl/Kadecot/wamp/KadecotWampDealer.java
@@ -3,8 +3,16 @@ package com.sonycsl.Kadecot.wamp;
 
 import com.sonycsl.wamp.WampDealer;
 import com.sonycsl.wamp.WampMessage;
+import com.sonycsl.wamp.WampRouter;
 
 public class KadecotWampDealer extends WampDealer {
+
+    public KadecotWampDealer() {
+    }
+
+    public KadecotWampDealer(WampRouter next) {
+        super(next);
+    }
 
     @Override
     protected void onConsumed(WampMessage msg) {
