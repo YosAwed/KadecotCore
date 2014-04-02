79f0e7855861967b80ea0e99acb58d0dcfa203b2
Yusuke Seshita <yseshita@users.noreply.github.com>
2014-04-02 12:55:16 +0900

    Add constructor of KadecotWampBroker

--- src/com/sonycsl/Kadecot/wamp/KadecotWampBroker.java
+++ src/com/sonycsl/Kadecot/wamp/KadecotWampBroker.java
@@ -3,11 +3,19 @@ package com.sonycsl.Kadecot.wamp;
 
 import com.sonycsl.wamp.WampBroker;
 import com.sonycsl.wamp.WampMessage;
+import com.sonycsl.wamp.WampRouter;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class KadecotWampBroker extends WampBroker {
+
+    public KadecotWampBroker() {
+    }
+
+    public KadecotWampBroker(WampRouter next) {
+        super(next);
+    }
 
     @Override
     protected JSONObject createEventDetails(JSONObject options, JSONArray arguments,
