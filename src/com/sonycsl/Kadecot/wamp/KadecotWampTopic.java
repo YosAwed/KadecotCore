81b7b300d8375c4cc0c1e56e5a67c4fc3a7b91bb
Yusuke Seshita <yseshita@users.noreply.github.com>
2014-04-09 11:59:13 +0900

    Add Kadecot Wamp Topic

---
+++ src/com/sonycsl/Kadecot/wamp/KadecotWampTopic.java
@@ -0,0 +1,10 @@
+
+package com.sonycsl.Kadecot.wamp;
+
+public class KadecotWampTopic {
+
+    public static final String TOPIC_DEVICE = "com.sonycsl.Kadecot.topic.device";
+
+    public static final String TOPIC_PRIVATE_DEVICE = "com.sonycsl.Kadecot.topic.private.device";
+
+}
