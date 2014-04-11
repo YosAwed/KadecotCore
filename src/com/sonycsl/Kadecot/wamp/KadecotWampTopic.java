d28c6a24ddb4553dd155d715e5a9cf9f222d67d2
Yusuke Seshita <yseshita@users.noreply.github.com>
2014-04-11 13:57:17 +0900

    Add private search topic

--- src/com/sonycsl/Kadecot/wamp/KadecotWampTopic.java
+++ src/com/sonycsl/Kadecot/wamp/KadecotWampTopic.java
@@ -7,4 +7,6 @@ public class KadecotWampTopic {
 
     public static final String TOPIC_PRIVATE_DEVICE = "com.sonycsl.Kadecot.topic.private.device";
 
+    public static final String TOPIC_PRIVATE_SEARCH = "com.sonycsl.Kadecot.topic.private.search";
+
 }
