b4448345efe350bb8e2a84acfdc0c41e7fb99adf
Yusuke Seshita <yseshita@users.noreply.github.com>
2014-06-05 11:41:26 +0900

    Include WAMP capability in Server handshake response

---
+++ src/com/sonycsl/kadecot/server/Draft_10_Protocol.java
@@ -0,0 +1,47 @@
+
+package com.sonycsl.kadecot.server;
+
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_10;
+import org.java_websocket.exceptions.InvalidHandshakeException;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.handshake.HandshakeBuilder;
+import org.java_websocket.handshake.ServerHandshakeBuilder;
+
+public class Draft_10_Protocol extends Draft_10 {
+
+    private static final String SEC_WS_PROTOCOL = "Sec-WebSocket-Protocol";
+
+    private String mSupportProtocol;
+
+    public Draft_10_Protocol(String protocol) {
+        mSupportProtocol = protocol;
+    }
+
+    @Override
+    public HandshakeState acceptHandshakeAsServer(ClientHandshake handshakedata)
+            throws InvalidHandshakeException {
+        if (super.acceptHandshakeAsServer(handshakedata) == HandshakeState.MATCHED) {
+            String protocol = handshakedata.getFieldValue(SEC_WS_PROTOCOL);
+            if (mSupportProtocol.equals(protocol)) {
+                return HandshakeState.MATCHED;
+            }
+        }
+
+        return HandshakeState.NOT_MATCHED;
+    }
+
+    @Override
+    public HandshakeBuilder postProcessHandshakeResponseAsServer(ClientHandshake request,
+            ServerHandshakeBuilder response) throws InvalidHandshakeException {
+        HandshakeBuilder builder = super.postProcessHandshakeResponseAsServer(request,
+                response);
+        builder.put(SEC_WS_PROTOCOL, mSupportProtocol);
+        return builder;
+    }
+
+    @Override
+    public Draft copyInstance() {
+        return new Draft_10_Protocol(mSupportProtocol);
+    }
+}
