44a47cfd736a1628527b2582b47cae9a77c9b653
Kosuke Mita <KosukeMita@users.noreply.github.com>
2014-06-18 15:49:40 +0900

    add WampHelloMessageImpl Unit test

---
+++ test/src/com/sonycsl/test/wamp/message/impl/WampHelloMessageImplTestCase.java
@@ -0,0 +1,55 @@
+
+package com.sonycsl.test.wamp.message.impl;
+
+import com.sonycsl.wamp.message.WampMessageType;
+import com.sonycsl.wamp.message.impl.WampHelloMessageImpl;
+
+import junit.framework.TestCase;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class WampHelloMessageImplTestCase extends TestCase {
+    private static final String REALM = "realm";
+
+    public void testCtor() {
+        try {
+            JSONArray msg = new JSONArray();
+            msg.put(WampMessageType.HELLO);
+            msg.put(REALM);
+            JSONObject role = new JSONObject("{\"roles\":{\"caller\":{}}}");
+            msg.put(role);
+            WampHelloMessageImpl hello = new WampHelloMessageImpl(msg);
+
+            assertNotNull(hello);
+            assertTrue(hello.isHelloMessage());
+            assertTrue(hello.getRealm().equals(REALM));
+            assertTrue(hello.getDetails() == role);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    public void testAbnormal() {
+        JSONArray msg = new JSONArray();
+        msg.put(WampMessageType.HELLO);
+        WampHelloMessageImpl hello = new WampHelloMessageImpl(msg);
+
+        // no realm
+        try {
+            String realm = hello.getRealm();
+            fail();
+        } catch (IllegalArgumentException e) {
+        }
+
+        // no details
+        try {
+            JSONObject details = hello.getDetails();
+            fail();
+        } catch (IllegalArgumentException e) {
+        }
+
+    }
+}
