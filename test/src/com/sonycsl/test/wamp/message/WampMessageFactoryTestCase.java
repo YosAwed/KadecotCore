291f66db3e5cab0b0f838559051826ee07fa6726
Kosuke Mita <KosukeMita@users.noreply.github.com>
2014-06-18 15:02:32 +0900

    add all kind message test by create() method

--- test/src/com/sonycsl/test/wamp/message/WampMessageFactoryTestCase.java
+++ test/src/com/sonycsl/test/wamp/message/WampMessageFactoryTestCase.java
@@ -3,6 +3,7 @@ package com.sonycsl.test.wamp.message;
 
 import com.sonycsl.wamp.WampError;
 import com.sonycsl.wamp.message.WampAbortMessage;
+import com.sonycsl.wamp.message.WampCallMessage;
 import com.sonycsl.wamp.message.WampErrorMessage;
 import com.sonycsl.wamp.message.WampEventMessage;
 import com.sonycsl.wamp.message.WampGoodbyeMessage;
@@ -15,6 +16,7 @@ import com.sonycsl.wamp.message.WampPublishMessage;
 import com.sonycsl.wamp.message.WampPublishedMessage;
 import com.sonycsl.wamp.message.WampRegisterMessage;
 import com.sonycsl.wamp.message.WampRegisteredMessage;
+import com.sonycsl.wamp.message.WampResultMessage;
 import com.sonycsl.wamp.message.WampSubscribeMessage;
 import com.sonycsl.wamp.message.WampSubscribedMessage;
 import com.sonycsl.wamp.message.WampUnregisterMessage;
@@ -39,11 +41,11 @@ public class WampMessageFactoryTestCase extends TestCase {
         assertNotNull(new WampMessageFactory());
     }
 
-    public void testCreate() {
+    public void testHelloCreate() {
         try {
             // create hello message
             JSONArray json = new JSONArray();
-            json.put(1);
+            json.put(WampMessageType.HELLO);
             json.put(REALM);
             JSONObject role = new JSONObject("{\"roles\":{\"caller\":{}}}");
             json.put(role);
@@ -60,6 +62,348 @@ public class WampMessageFactoryTestCase extends TestCase {
             e.printStackTrace();
             fail();
         }
+    }
+
+    public void testWelcomeCreate() {
+        try {
+            JSONArray json = new JSONArray();
+            json.put(WampMessageType.WELCOME);
+            int sessionId = 1;
+            json.put(sessionId);
+            JSONObject role = new JSONObject("{\"roles\":{\"dealer\":{}}}");
+            json.put(role);
+
+            WampMessage msg = WampMessageFactory.create(json);
+
+            assertTrue(msg.isWelcomeMessage());
+
+            WampWelcomeMessage welcome = msg.asWelcomeMessage();
+            assertTrue(welcome.getSession() == sessionId);
+            assertTrue(welcome.getDetails() == role);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    public void testAbortCreate() {
+        JSONArray json = new JSONArray();
+        json.put(WampMessageType.ABORT);
+        JSONObject details = new JSONObject();
+        json.put(details);
+        String reason = WampError.NO_SUCH_REALM;
+        json.put(reason);
+
+        WampMessage msg = WampMessageFactory.create(json);
+
+        assertTrue(msg.isAbortMessage());
+
+        WampAbortMessage abort = msg.asAbortMessage();
+        assertTrue(abort.getDetails() == details);
+        assertTrue(abort.getReason().equals(reason));
+    }
+
+    public void testGoodbyeCreate() {
+        JSONArray json = new JSONArray();
+        json.put(WampMessageType.GOODBYE);
+        JSONObject details = new JSONObject();
+        json.put(details);
+        String reason = WampError.GOODBYE_AND_OUT;
+        json.put(reason);
+
+        WampMessage msg = WampMessageFactory.create(json);
+
+        assertTrue(msg.isGoodbyeMessage());
+
+        WampGoodbyeMessage goodbye = msg.asGoodbyeMessage();
+        assertTrue(goodbye.getDetails() == details);
+        assertTrue(goodbye.getReason().equals(reason));
+    }
+
+    public void testErrorCreate() {
+        JSONArray json = new JSONArray();
+        json.put(WampMessageType.ERROR);
+        int requestType = WampMessageType.CALL;
+        json.put(requestType);
+        int requestId = 1;
+        json.put(requestId);
+        JSONObject details = new JSONObject();
+        json.put(details);
+        String error = WampError.NO_SUCH_PROCEDURE;
+        json.put(error);
+
+        WampMessage msg = WampMessageFactory.create(json);
+
+        assertTrue(msg.isErrorMessage());
+
+        WampErrorMessage errorMsg = msg.asErrorMessage();
+        assertTrue(errorMsg.getRequestType() == requestType);
+        assertTrue(errorMsg.getRequestId() == requestId);
+        assertTrue(errorMsg.getDetails() == details);
+        assertTrue(errorMsg.getUri().equals(error));
+    }
+
+    public void testPublishCreate() {
+        JSONArray json = new JSONArray();
+        json.put(WampMessageType.PUBLISH);
+        int requestId = 1;
+        json.put(requestId);
+        JSONObject options = new JSONObject();
+        json.put(options);
+        json.put(TOPIC);
+
+        WampMessage msg = WampMessageFactory.create(json);
+
+        assertTrue(msg.isPublishMessage());
+
+        WampPublishMessage publish = msg.asPublishMessage();
+        assertTrue(publish.getRequestId() == requestId);
+        assertTrue(publish.getOptions() == options);
+        assertTrue(publish.getTopic().equals(TOPIC));
+    }
+
+    public void testPublishedCreate() {
+        JSONArray json = new JSONArray();
+        json.put(WampMessageType.PUBLISHED);
+        int requestId = 1;
+        json.put(requestId);
+        int publicationId = 2;
+        json.put(publicationId);
+
+        WampMessage msg = WampMessageFactory.create(json);
+
+        assertTrue(msg.isPublishedMessage());
+
+        WampPublishedMessage published = msg.asPublishedMessage();
+        assertTrue(published.getRequestId() == requestId);
+        assertTrue(published.getPublicationId() == publicationId);
+    }
+
+    public void testSubscribeCreate() {
+        JSONArray json = new JSONArray();
+        json.put(WampMessageType.SUBSCRIBE);
+        int requestId = 1;
+        json.put(requestId);
+        JSONObject options = new JSONObject();
+        json.put(options);
+        json.put(TOPIC);
+
+        WampMessage msg = WampMessageFactory.create(json);
+
+        assertTrue(msg.isSubscribeMessage());
+
+        WampSubscribeMessage subscribe = msg.asSubscribeMessage();
+        assertTrue(subscribe.getRequestId() == requestId);
+        assertTrue(subscribe.getOptions() == options);
+        assertTrue(subscribe.getTopic().equals(TOPIC));
+    }
+
+    public void testSubscribedCreate() {
+        JSONArray json = new JSONArray();
+        json.put(WampMessageType.SUBSCRIBED);
+        int requestId = 1;
+        json.put(requestId);
+        int subscriptionId = 2;
+        json.put(subscriptionId);
+
+        WampMessage msg = WampMessageFactory.create(json);
+
+        assertTrue(msg.isSubscribedMessage());
+
+        WampSubscribedMessage subscribed = msg.asSubscribedMessage();
+        assertTrue(subscribed.getRequestId() == requestId);
+        assertTrue(subscribed.getSubscriptionId() == subscriptionId);
+    }
+
+    public void testUnsubscribeCreate() {
+        JSONArray json = new JSONArray();
+        json.put(WampMessageType.UNSUBSCRIBE);
+        int requestId = 1;
+        json.put(requestId);
+        int subscriptionId = 2;
+        json.put(subscriptionId);
+
+        WampMessage msg = WampMessageFactory.create(json);
+
+        assertTrue(msg.isUnsubscribeMessage());
+
+        WampUnsubscribeMessage unsubscribe = msg.asUnsubscribeMessage();
+        assertTrue(unsubscribe.getRequestId() == requestId);
+        assertTrue(unsubscribe.getSubscriptionId() == subscriptionId);
+    }
+
+    public void testUnsubscribedCreate() {
+        JSONArray json = new JSONArray();
+        json.put(WampMessageType.UNSUBSCRIBED);
+        int requestId = 1;
+        json.put(requestId);
+
+        WampMessage msg = WampMessageFactory.create(json);
+
+        assertTrue(msg.isUnsubscribedMessage());
+
+        WampUnsubscribedMessage unsubscribed = msg.asUnsubscribedMessage();
+        assertTrue(unsubscribed.getRequestId() == requestId);
+    }
+
+    public void testEventCreate() {
+        JSONArray json = new JSONArray();
+        json.put(WampMessageType.EVENT);
+        int subscriptionId = 1;
+        json.put(subscriptionId);
+        int publicationId = 2;
+        json.put(publicationId);
+        JSONObject details = new JSONObject();
+        json.put(details);
+
+        WampMessage msg = WampMessageFactory.create(json);
+
+        assertTrue(msg.isEventMessage());
+
+        WampEventMessage event = msg.asEventMessage();
+        assertTrue(event.getSubscriptionId() == subscriptionId);
+        assertTrue(event.getPublicationId() == publicationId);
+        assertTrue(event.getDetails() == details);
+    }
+
+    public void testCallCreate() {
+        JSONArray json = new JSONArray();
+        json.put(WampMessageType.CALL);
+        int requestId = 1;
+        json.put(requestId);
+        JSONObject options = new JSONObject();
+        json.put(options);
+        json.put(PROCEDURE);
+
+        WampMessage msg = WampMessageFactory.create(json);
+
+        assertTrue(msg.isCallMessage());
+
+        WampCallMessage call = msg.asCallMessage();
+        assertTrue(call.getRequestId() == requestId);
+        assertTrue(call.getOptions() == options);
+        assertTrue(call.getProcedure().equals(PROCEDURE));
+    }
+
+    public void testResultCreate() {
+        JSONArray json = new JSONArray();
+        json.put(WampMessageType.RESULT);
+        int requestId = 1;
+        json.put(requestId);
+        JSONObject details = new JSONObject();
+        json.put(details);
+
+        WampMessage msg = WampMessageFactory.create(json);
+
+        assertTrue(msg.isResultMessage());
+
+        WampResultMessage result = msg.asResultMessage();
+        assertTrue(result.getRequestId() == requestId);
+        assertTrue(result.getDetails() == details);
+    }
+
+    public void testRegisterCreate() {
+        JSONArray json = new JSONArray();
+        json.put(WampMessageType.REGISTER);
+        int requestId = 1;
+        json.put(requestId);
+        JSONObject options = new JSONObject();
+        json.put(options);
+        json.put(PROCEDURE);
+
+        WampMessage msg = WampMessageFactory.create(json);
+
+        assertTrue(msg.isRegisterMessage());
+
+        WampRegisterMessage register = msg.asRegisterMessage();
+        assertTrue(register.getRequestId() == requestId);
+        assertTrue(register.getOptions() == options);
+        assertTrue(register.getProcedure().equals(PROCEDURE));
+    }
+
+    public void testRegisteredCreate() {
+        JSONArray json = new JSONArray();
+        json.put(WampMessageType.REGISTERED);
+        int requestId = 1;
+        json.put(requestId);
+        int registrationId = 2;
+        json.put(registrationId);
+
+        WampMessage msg = WampMessageFactory.create(json);
+
+        assertTrue(msg.isRegisteredMessage());
+
+        WampRegisteredMessage registered = msg.asRegisteredMessage();
+        assertTrue(registered.getRequestId() == requestId);
+        assertTrue(registered.getRegistrationId() == registrationId);
+    }
+
+    public void testUnregisterCreate() {
+        JSONArray json = new JSONArray();
+        json.put(WampMessageType.UNREGISTER);
+        int requestId = 1;
+        json.put(requestId);
+        int registrationId = 2;
+        json.put(registrationId);
+
+        WampMessage msg = WampMessageFactory.create(json);
+
+        assertTrue(msg.isUnregisterMessage());
+
+        WampUnregisterMessage unregister = msg.asUnregisterMessage();
+        assertTrue(unregister.getRequestId() == requestId);
+        assertTrue(unregister.getRegistrationId() == registrationId);
+    }
+
+    public void testUnregsiteredCreate() {
+        JSONArray json = new JSONArray();
+        json.put(WampMessageType.UNREGISTERED);
+        int requestId = 1;
+        json.put(requestId);
+
+        WampMessage msg = WampMessageFactory.create(json);
+
+        assertTrue(msg.isUnregisteredMessage());
+
+        WampUnregisteredMessage unregistered = msg.asUnregisteredMessage();
+        assertTrue(unregistered.getRequestId() == requestId);
+    }
+
+    public void testInvocationCreate() {
+        JSONArray json = new JSONArray();
+        json.put(WampMessageType.INVOCATION);
+        int requestId = 1;
+        json.put(requestId);
+        int registrationId = 2;
+        json.put(registrationId);
+        JSONObject details = new JSONObject();
+        json.put(details);
+
+        WampMessage msg = WampMessageFactory.create(json);
+
+        assertTrue(msg.isInvocationMessage());
+
+        WampInvocationMessage invocation = msg.asInvocationMessage();
+        assertTrue(invocation.getRequestId() == requestId);
+        assertTrue(invocation.getRegistrationId() == registrationId);
+        assertTrue(invocation.getDetails() == details);
+    }
+
+    public void testYieldCreate() {
+        JSONArray json = new JSONArray();
+        json.put(WampMessageType.YIELD);
+        int requestId = 1;
+        json.put(requestId);
+        JSONObject options = new JSONObject();
+        json.put(options);
+
+        WampMessage msg = WampMessageFactory.create(json);
+
+        assertTrue(msg.isYieldMessage());
+
+        WampYieldMessage yield = msg.asYieldMessage();
+        assertTrue(yield.getRequestId() == requestId);
+        assertTrue(yield.getOptions() == options);
     }
 
     public void testCreateAbnormal() {
