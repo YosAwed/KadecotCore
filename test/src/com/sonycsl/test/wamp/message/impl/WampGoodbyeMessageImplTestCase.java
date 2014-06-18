32df986aa2c005bcfde42f6f8613fa9c38f25cdf
Kosuke Mita <KosukeMita@users.noreply.github.com>
2014-06-18 16:17:42 +0900

    add WampGoodbyetMessageImpl Unit test

---
+++ test/src/com/sonycsl/test/wamp/message/impl/WampGoodbyeMessageImplTestCase.java
@@ -0,0 +1,48 @@
+
+package com.sonycsl.test.wamp.message.impl;
+
+import com.sonycsl.wamp.WampError;
+import com.sonycsl.wamp.message.WampMessageType;
+import com.sonycsl.wamp.message.impl.WampGoodbyeMessageImpl;
+
+import junit.framework.TestCase;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class WampGoodbyeMessageImplTestCase extends TestCase {
+    public void testCotr() {
+        JSONArray msg = new JSONArray();
+        msg.put(WampMessageType.GOODBYE);
+        JSONObject details = new JSONObject();
+        msg.put(details);
+        String reason = WampError.GOODBYE_AND_OUT;
+        msg.put(reason);
+
+        WampGoodbyeMessageImpl goodbye = new WampGoodbyeMessageImpl(msg);
+
+        assertNotNull(goodbye);
+        assertTrue(goodbye.isGoodbyeMessage());
+        assertTrue(goodbye.getDetails() == details);
+        assertTrue(goodbye.getReason().equals(reason));
+    }
+
+    public void testAbnormal() {
+        JSONArray msg = new JSONArray();
+        WampGoodbyeMessageImpl goodbye = new WampGoodbyeMessageImpl(msg);
+
+        // no details
+        try {
+            JSONObject details = goodbye.getDetails();
+            fail();
+        } catch (Exception e) {
+        }
+
+        // no reason
+        try {
+            String reason = goodbye.getReason();
+            fail();
+        } catch (Exception e) {
+        }
+    }
+}
