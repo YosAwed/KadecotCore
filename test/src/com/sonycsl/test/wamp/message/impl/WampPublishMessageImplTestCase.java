cd3d307b2a754a5540dc00a0be32c2a884eb07ad
Kosuke Mita <KosukeMita@users.noreply.github.com>
2014-06-18 17:04:18 +0900

    add WampPublishMessageImpl Unit test

---
+++ test/src/com/sonycsl/test/wamp/message/impl/WampPublishMessageImplTestCase.java
@@ -0,0 +1,81 @@
+
+package com.sonycsl.test.wamp.message.impl;
+
+import com.sonycsl.wamp.message.WampMessageType;
+import com.sonycsl.wamp.message.impl.WampPublishMessageImpl;
+
+import junit.framework.TestCase;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class WampPublishMessageImplTestCase extends TestCase {
+    public void testCtor() {
+        JSONArray msg = new JSONArray();
+        msg.put(WampMessageType.PUBLISH);
+        int requestId = 1;
+        msg.put(requestId);
+        JSONObject options = new JSONObject();
+        msg.put(options);
+        String topic = "topic.test";
+        msg.put(topic);
+        JSONArray arguments = new JSONArray();
+        msg.put(arguments);
+        JSONObject argumentsKw = new JSONObject();
+        msg.put(argumentsKw);
+
+        WampPublishMessageImpl publish = new WampPublishMessageImpl(msg);
+
+        assertNotNull(publish);
+        assertTrue(publish.isPublishMessage());
+        assertTrue(publish.getRequestId() == requestId);
+        assertTrue(publish.getOptions() == options);
+        assertTrue(publish.hasArguments());
+        assertTrue(publish.getArguments() == arguments);
+        assertTrue(publish.hasArgumentsKw());
+        assertTrue(publish.getArgumentsKw() == argumentsKw);
+    }
+
+    public void testAbnormal() {
+        JSONArray msg = new JSONArray();
+        WampPublishMessageImpl publish = new WampPublishMessageImpl(msg);
+
+        // no requestId
+        try {
+            int requestId = publish.getRequestId();
+            fail();
+        } catch (IllegalArgumentException e) {
+        }
+
+        // no options
+        try {
+            JSONObject options = publish.getOptions();
+            fail();
+        } catch (IllegalArgumentException e) {
+        }
+
+        // no topic
+        try {
+            String topic = publish.getTopic();
+            fail();
+        } catch (IllegalArgumentException e) {
+        }
+
+        // no arguments
+        try {
+            assertFalse(publish.hasArguments());
+            JSONArray arguments = publish.getArguments();
+            fail();
+        } catch (IllegalArgumentException e) {
+        }
+
+        // no argumentsKw
+        try {
+            assertFalse(publish.hasArgumentsKw());
+            JSONObject argumentsKw = publish.getArgumentsKw();
+            fail();
+        } catch (IllegalArgumentException e) {
+        }
+
+    }
+}
