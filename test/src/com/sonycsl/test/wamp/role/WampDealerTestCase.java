3427b9546521ce7329b8329f3a1f82d852cf259a
Kosuke Mita <KosukeMita@users.noreply.github.com>
2014-06-19 21:27:23 +0900

    add tests to WampDealer Unit test
    - tests to expand coverage
    - no such procedure test as abnormal test
    - high load register test

--- test/src/com/sonycsl/test/wamp/role/WampDealerTestCase.java
+++ test/src/com/sonycsl/test/wamp/role/WampDealerTestCase.java
@@ -57,6 +57,10 @@ public class WampDealerTestCase extends TestCase {
         assertNotNull(mPeer3);
     }
 
+    public void testGetRoleName() {
+        assertEquals(mDealer.getRoleName(), "dealer");
+    }
+
     public void testRxRegister() {
         final CountDownLatch latch = new CountDownLatch(1);
         assertTrue(mDealer.resolveRxMessage(mPeer1, WampMessageFactory.createRegister(
@@ -314,6 +318,140 @@ public class WampDealerTestCase extends TestCase {
         }
     }
 
+    public void testRxYieldWithArguments() {
+        final CountDownLatch latch = new CountDownLatch(1);
+        assertTrue(mDealer.resolveRxMessage(mPeer1, WampMessageFactory.createRegister(
+                WampRequestIdGenerator.getId(), new JSONObject(), PROCEDURE1),
+                new OnReplyListener() {
+                    @Override
+                    public void onReply(WampPeer receiver, WampMessage reply) {
+                        if (reply.isRegisteredMessage()) {
+                            assertEquals(mPeer1, receiver);
+                            latch.countDown();
+                            return;
+                        }
+                        fail();
+                    }
+                }));
+        try {
+            assertTrue(latch.await(1, TimeUnit.SECONDS));
+        } catch (InterruptedException e) {
+            fail();
+        }
+
+        final CountDownLatch callLatch = new CountDownLatch(1);
+        final AtomicInteger requestId = new AtomicInteger(-1);
+        assertTrue(mDealer.resolveRxMessage(mPeer2,
+                WampMessageFactory.createCall(WampRequestIdGenerator.getId(), new JSONObject(),
+                        PROCEDURE1), new OnReplyListener() {
+                    @Override
+                    public void onReply(WampPeer receiver, WampMessage reply) {
+                        if (reply.isInvocationMessage()) {
+                            assertEquals(mPeer1, receiver);
+                            requestId.set(reply.asInvocationMessage().getRequestId());
+                            assertTrue(requestId.get() > 0);
+                            callLatch.countDown();
+                            return;
+                        }
+                        fail();
+                    }
+                }));
+        try {
+            assertTrue(callLatch.await(1, TimeUnit.SECONDS));
+        } catch (InterruptedException e) {
+            fail();
+        }
+
+        int val = 1;
+        final CountDownLatch yieldLatch = new CountDownLatch(1);
+        assertTrue(mDealer.resolveRxMessage(
+                mPeer1, WampMessageFactory.createYield(requestId.get(), new JSONObject(),
+                        new JSONArray().put(val)), new OnReplyListener() {
+
+                    @Override
+                    public void onReply(WampPeer receiver, WampMessage reply) {
+                        if (reply.isResultMessage() && reply.asResultMessage().hasArguments()) {
+                            assertEquals(receiver, mPeer2);
+                            yieldLatch.countDown();
+                            return;
+                        }
+                        fail();
+                    }
+                }));
+        try {
+            assertTrue(yieldLatch.await(1, TimeUnit.SECONDS));
+        } catch (InterruptedException e) {
+            fail();
+        }
+    }
+
+    public void testRxYieldWithArgumentsKw() {
+        final CountDownLatch latch = new CountDownLatch(1);
+        assertTrue(mDealer.resolveRxMessage(mPeer1, WampMessageFactory.createRegister(
+                WampRequestIdGenerator.getId(), new JSONObject(), PROCEDURE1),
+                new OnReplyListener() {
+                    @Override
+                    public void onReply(WampPeer receiver, WampMessage reply) {
+                        if (reply.isRegisteredMessage()) {
+                            assertEquals(mPeer1, receiver);
+                            latch.countDown();
+                            return;
+                        }
+                        fail();
+                    }
+                }));
+        try {
+            assertTrue(latch.await(1, TimeUnit.SECONDS));
+        } catch (InterruptedException e) {
+            fail();
+        }
+
+        final CountDownLatch callLatch = new CountDownLatch(1);
+        final AtomicInteger requestId = new AtomicInteger(-1);
+        assertTrue(mDealer.resolveRxMessage(mPeer2,
+                WampMessageFactory.createCall(WampRequestIdGenerator.getId(), new JSONObject(),
+                        PROCEDURE1), new OnReplyListener() {
+                    @Override
+                    public void onReply(WampPeer receiver, WampMessage reply) {
+                        if (reply.isInvocationMessage()) {
+                            assertEquals(mPeer1, receiver);
+                            requestId.set(reply.asInvocationMessage().getRequestId());
+                            assertTrue(requestId.get() > 0);
+                            callLatch.countDown();
+                            return;
+                        }
+                        fail();
+                    }
+                }));
+        try {
+            assertTrue(callLatch.await(1, TimeUnit.SECONDS));
+        } catch (InterruptedException e) {
+            fail();
+        }
+
+        final CountDownLatch yieldLatch = new CountDownLatch(1);
+        assertTrue(mDealer.resolveRxMessage(
+                mPeer1, WampMessageFactory.createYield(requestId.get(), new JSONObject(),
+                        new JSONArray(), new JSONObject()), new OnReplyListener() {
+
+                    @Override
+                    public void onReply(WampPeer receiver, WampMessage reply) {
+                        if (reply.isResultMessage() && reply.asResultMessage().hasArguments() &&
+                                reply.asResultMessage().hasArgumentsKw()) {
+                            assertEquals(receiver, mPeer2);
+                            yieldLatch.countDown();
+                            return;
+                        }
+                        fail();
+                    }
+                }));
+        try {
+            assertTrue(yieldLatch.await(1, TimeUnit.SECONDS));
+        } catch (InterruptedException e) {
+            fail();
+        }
+    }
+
     public void testRxError() {
         final CountDownLatch latch = new CountDownLatch(1);
         assertTrue(mDealer.resolveRxMessage(mPeer1, WampMessageFactory.createRegister(
@@ -381,6 +519,140 @@ public class WampDealerTestCase extends TestCase {
         }
     }
 
+    public void testRxErrorWithArguments() {
+        final CountDownLatch latch = new CountDownLatch(1);
+        assertTrue(mDealer.resolveRxMessage(mPeer1, WampMessageFactory.createRegister(
+                WampRequestIdGenerator.getId(), new JSONObject(), PROCEDURE1),
+                new OnReplyListener() {
+                    @Override
+                    public void onReply(WampPeer receiver, WampMessage reply) {
+                        if (reply.isRegisteredMessage()) {
+                            assertEquals(mPeer1, receiver);
+                            latch.countDown();
+                            return;
+                        }
+                        fail();
+                    }
+                }));
+        try {
+            assertTrue(latch.await(1, TimeUnit.SECONDS));
+        } catch (InterruptedException e) {
+            fail();
+        }
+
+        final CountDownLatch callLatch = new CountDownLatch(1);
+        final AtomicInteger requestId = new AtomicInteger(-1);
+        assertTrue(mDealer.resolveRxMessage(mPeer2,
+                WampMessageFactory.createCall(WampRequestIdGenerator.getId(), new JSONObject(),
+                        PROCEDURE1), new OnReplyListener() {
+                    @Override
+                    public void onReply(WampPeer receiver, WampMessage reply) {
+                        if (reply.isInvocationMessage()) {
+                            assertEquals(mPeer1, receiver);
+                            requestId.set(reply.asInvocationMessage().getRequestId());
+                            assertTrue(requestId.get() > 0);
+                            callLatch.countDown();
+                            return;
+                        }
+                        fail();
+                    }
+                }));
+        try {
+            assertTrue(callLatch.await(1, TimeUnit.SECONDS));
+        } catch (InterruptedException e) {
+            fail();
+        }
+
+        final CountDownLatch errorLatch = new CountDownLatch(1);
+        assertTrue(mDealer.resolveRxMessage(mPeer1, WampMessageFactory.createError(
+                WampMessageType.INVOCATION, requestId.get(), new JSONObject(),
+                WampError.INVALID_ARGUMENT, new JSONArray()),
+                new OnReplyListener() {
+                    @Override
+                    public void onReply(WampPeer receiver, WampMessage reply) {
+                        if (reply.isErrorMessage()) {
+                            assertEquals(receiver, mPeer2);
+                            if (reply.asErrorMessage().getRequestType() == WampMessageType.CALL) {
+                                errorLatch.countDown();
+                                return;
+                            }
+                        }
+                    }
+                }));
+        try {
+            assertTrue(errorLatch.await(1, TimeUnit.SECONDS));
+        } catch (InterruptedException e) {
+            fail();
+        }
+    }
+
+    public void testRxErrorWithArgumentsKw() {
+        final CountDownLatch latch = new CountDownLatch(1);
+        assertTrue(mDealer.resolveRxMessage(mPeer1, WampMessageFactory.createRegister(
+                WampRequestIdGenerator.getId(), new JSONObject(), PROCEDURE1),
+                new OnReplyListener() {
+                    @Override
+                    public void onReply(WampPeer receiver, WampMessage reply) {
+                        if (reply.isRegisteredMessage()) {
+                            assertEquals(mPeer1, receiver);
+                            latch.countDown();
+                            return;
+                        }
+                        fail();
+                    }
+                }));
+        try {
+            assertTrue(latch.await(1, TimeUnit.SECONDS));
+        } catch (InterruptedException e) {
+            fail();
+        }
+
+        final CountDownLatch callLatch = new CountDownLatch(1);
+        final AtomicInteger requestId = new AtomicInteger(-1);
+        assertTrue(mDealer.resolveRxMessage(mPeer2,
+                WampMessageFactory.createCall(WampRequestIdGenerator.getId(), new JSONObject(),
+                        PROCEDURE1), new OnReplyListener() {
+                    @Override
+                    public void onReply(WampPeer receiver, WampMessage reply) {
+                        if (reply.isInvocationMessage()) {
+                            assertEquals(mPeer1, receiver);
+                            requestId.set(reply.asInvocationMessage().getRequestId());
+                            assertTrue(requestId.get() > 0);
+                            callLatch.countDown();
+                            return;
+                        }
+                        fail();
+                    }
+                }));
+        try {
+            assertTrue(callLatch.await(1, TimeUnit.SECONDS));
+        } catch (InterruptedException e) {
+            fail();
+        }
+
+        final CountDownLatch errorLatch = new CountDownLatch(1);
+        assertTrue(mDealer.resolveRxMessage(mPeer1, WampMessageFactory.createError(
+                WampMessageType.INVOCATION, requestId.get(), new JSONObject(),
+                WampError.INVALID_ARGUMENT, new JSONArray(), new JSONObject()),
+                new OnReplyListener() {
+                    @Override
+                    public void onReply(WampPeer receiver, WampMessage reply) {
+                        if (reply.isErrorMessage()) {
+                            assertEquals(receiver, mPeer2);
+                            if (reply.asErrorMessage().getRequestType() == WampMessageType.CALL) {
+                                errorLatch.countDown();
+                                return;
+                            }
+                        }
+                    }
+                }));
+        try {
+            assertTrue(errorLatch.await(1, TimeUnit.SECONDS));
+        } catch (InterruptedException e) {
+            fail();
+        }
+    }
+
     public void testResolveTxMessage() {
         try {
             mDealer.resolveTxMessage(null, null);
@@ -389,5 +661,96 @@ public class WampDealerTestCase extends TestCase {
         }
 
         fail();
+    }
+
+    // abnormal
+    public void testNoSuchProcedure() {
+        mDealer.resolveRxMessage(mPeer1,
+                WampMessageFactory.createCall(1, new JSONObject(), "test"), new OnReplyListener() {
+
+                    @Override
+                    public void onReply(WampPeer receiver, WampMessage reply) {
+                        assertTrue(reply.isErrorMessage());
+                        assertEquals(WampError.NO_SUCH_PROCEDURE, reply.asErrorMessage().getUri());
+                    }
+                });
+    }
+
+    public void testRxManyRegister() {
+        int regNum = 200;
+        final CountDownLatch latch2 = new CountDownLatch(regNum);
+
+        // register procedure1 * 100
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    mDealer.resolveRxMessage(mPeer1, WampMessageFactory.createRegister(
+                            WampRequestIdGenerator.getId(), new JSONObject(), PROCEDURE1 + i),
+                            new OnReplyListener() {
+                                @Override
+                                public void onReply(WampPeer receiver, WampMessage reply) {
+                                    if (reply.isRegisteredMessage()) {
+                                        latch2.countDown();
+                                        return;
+                                    }
+                                    fail();
+                                }
+                            });
+                }
+            }
+        }).start();
+
+        // register procedure2 * 100
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    mDealer.resolveRxMessage(mPeer2, WampMessageFactory.createRegister(
+                            WampRequestIdGenerator.getId(), new JSONObject(), PROCEDURE2 + i),
+                            new OnReplyListener() {
+                                @Override
+                                public void onReply(WampPeer receiver, WampMessage reply) {
+                                    if (reply.isRegisteredMessage()) {
+                                        latch2.countDown();
+                                        return;
+                                    }
+                                    fail(reply.toString());
+                                }
+                            });
+                }
+            }
+        }).start();
+
+        try {
+            assertTrue(latch2.await(5, TimeUnit.SECONDS));
+        } catch (InterruptedException e) {
+            fail();
+        }
+
+        // call procedures
+        for (int i = 0; i < regNum / 2; i++) {
+            assertTrue(mDealer.resolveRxMessage(mPeer1, WampMessageFactory.createCall(
+                    WampRequestIdGenerator.getId(), new JSONObject(), PROCEDURE1 + i),
+                    new OnReplyListener() {
+                        @Override
+                        public void onReply(WampPeer receiver, WampMessage reply) {
+                            if (reply.isErrorMessage()) {
+                                fail();
+                            }
+                        }
+                    }));
+
+            assertTrue(mDealer.resolveRxMessage(mPeer2, WampMessageFactory.createCall(
+                    WampRequestIdGenerator.getId(), new JSONObject(), PROCEDURE2 + i),
+                    new OnReplyListener() {
+                        @Override
+                        public void onReply(WampPeer receiver, WampMessage reply) {
+                            if (reply.isErrorMessage()) {
+                                fail();
+                            }
+                        }
+                    }));
+        }
     }
 }
